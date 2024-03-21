3d8cc0140f97628b6460f75ababfa724c7fe2457
oshumylo <oshumylo.appwell@ancestry.com>
2024-03-22 00:35:48 +0200

    remove unused url

--- PageViewController/ContentView.swift
+++ PageViewController/ContentView.swift
@@ -9,11 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let imageUrl1 = "https://mediasvc.ancestrystage.com/v2/image/namespaces/1093/media/4cc0bbbd-b908-4105-b198-17c3de9e50c6.jpg?Client=AncestryIOS&MaxSide=400"
     let imageURl2 = "https://mediasvc.ancestry.com/v2/image/namespaces/1093/media/70fc088a-bc84-42b9-8828-5d685cc89924.jpg?Client=AncestryIOS&MaxSide=1000&ms_params="
     
     var body: some View {
-        PageViewControllerContainer([
+        PageViewControllerContainer(pages: [
             .audio("TestFile"),
             .image(imageURl2),
             .link(imageURl2, "TestFile")
