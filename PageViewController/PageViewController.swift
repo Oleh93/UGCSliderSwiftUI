590f42b01e49f7e8959480ebf2c19b1619176dd7
oshumylo <oshumylo.appwell@ancestry.com>
2024-03-22 00:35:25 +0200

    change UIKIT pager with SwiftUI pager

--- PageViewController/PageViewController.swift
+++ PageViewController/PageViewController.swift
@@ -17,12 +17,15 @@ enum PageType {
 struct PageViewControllerContainer: View {
     
     var pages: [PageType]
-    init(_ pages: [PageType]) {
-        self.pages = pages
-    }
     
     var body: some View {
-        PageViewController(controllers: pages.map({ UIHostingController(rootView: getView(for: $0)) }))
+        TabView {
+            ForEach(pages.indices, id: \.self) { index in
+                getView(for: pages[index])
+                    .tag(index)
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
     }
     
     @ViewBuilder
@@ -36,47 +39,4 @@ struct PageViewControllerContainer: View {
             LinkPageView(imageURL: imageURL, audioURL: audioURL)
         }
     }
-}
-
-struct PageViewController: UIViewControllerRepresentable {
-    func makeCoordinator() -> Coordinator {
-        return Coordinator(self)
-    }
-    
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
-        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-            guard let index = self.parent.controllers.firstIndex(of: viewController) else { return nil }
-            if index == 0 {
-                return self.parent.controllers.last
-            }
-            return self.parent.controllers[index - 1]
-        }
-
-        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-            guard let index = self.parent.controllers.firstIndex(of: viewController) else { return nil }
-            if index == self.parent.controllers.count - 1 {
-                return self.parent.controllers.first
-            }
-            return self.parent.controllers[index + 1]
-        }
-        
-        let parent: PageViewController
-        init(_ parent: PageViewController) {
-            self.parent = parent
-        }
-    }
-    
-    func makeUIViewController(context: Context) -> UIPageViewController {
-        let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
-        pageViewController.dataSource = context.coordinator
-        return pageViewController
-    }
-    
-    func updateUIViewController(_ uiViewController: UIPageViewController, context: Context) {
-        uiViewController.setViewControllers([controllers[0]], direction: .forward, animated: true)
-    }
-    
-    typealias UIViewControllerType = UIPageViewController
-    
-    var controllers: [UIViewController] = []
 }
