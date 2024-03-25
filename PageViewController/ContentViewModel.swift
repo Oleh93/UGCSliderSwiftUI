5a03fb50c346829c2029b933e69bb39a3398f30e
Oleh Mykytyn <omykytyn.appwell@OMYKYTYN-2.local>
2024-03-25 11:49:31 +0100

    after rebase fix

--- PageViewController/ContentViewModel.swift
+++ PageViewController/ContentViewModel.swift
@@ -35,7 +35,79 @@ class ContentViewModel {
             )),
             .audio(audioName),
             .link(imageUrl2, audioName),
-            .video(videoName),
+            .video(videoUrl?.absoluteString ?? ""),
+            
+                .image(.init(
+                    url: imageUrl3,
+                    toolBarItems: [
+                        .init(
+                            image: .init(systemName: "info.circle"),
+                            action: { self.state.isShowingInfoAlert = true }
+                        ),
+                        .init(
+                            image: .init(systemName: "square.and.arrow.up"),
+                            action: { }
+                        ),
+                        .init(
+                            image: .init(systemName: "trash"),
+                            action: { self.state.isShowingDeleteAlert = true }
+                        )
+                    ]
+                )),
+            .image(.init(
+                url: imageUrl4,
+                toolBarItems: [
+                    .init(
+                        image: .init(systemName: "info.circle"),
+                        action: { self.state.isShowingInfoAlert = true }
+                    ),
+                    .init(
+                        image: .init(systemName: "square.and.arrow.up"),
+                        action: { }
+                    ),
+                    .init(
+                        image: .init(systemName: "trash"),
+                        action: { self.state.isShowingDeleteAlert = true }
+                    )
+                ]
+            )),
+            .image(.init(
+                url: imageUrl5,
+                toolBarItems: [
+                    .init(
+                        image: .init(systemName: "info.circle"),
+                        action: { self.state.isShowingInfoAlert = true }
+                    ),
+                    .init(
+                        image: .init(systemName: "square.and.arrow.up"),
+                        action: { }
+                    ),
+                    .init(
+                        image: .init(systemName: "trash"),
+                        action: { self.state.isShowingDeleteAlert = true }
+                    )
+                ]
+            )),
+            
+                .video(videoUrl2?.absoluteString ?? ""),
+            
+                .image(.init(
+                    url: imageUrl6,
+                    toolBarItems: [
+                        .init(
+                            image: .init(systemName: "info.circle"),
+                            action: { self.state.isShowingInfoAlert = true }
+                        ),
+                        .init(
+                            image: .init(systemName: "square.and.arrow.up"),
+                            action: { }
+                        ),
+                        .init(
+                            image: .init(systemName: "trash"),
+                            action: { self.state.isShowingDeleteAlert = true }
+                        )
+                    ]
+                )),
         ]
         
         state.configurePages(pages)
